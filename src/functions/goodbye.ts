598603cb4cdcd413a412610966156056354543e5
ziad harb <ziad.harb@pennymac.com>
2023-08-29 12:27:05 -0500

    initial commit

--- src/functions/goodbye.ts
+++ src/functions/goodbye.ts
@@ -1,12 +1,16 @@
-import { SNSHandler, SNSEvent, SNSEventRecord } from 'aws-lambda';
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 
-/*
-    SNS Topics don't expect a return value. Usually you either publish to another topic or just do some work and finish
-*/
-export const handler: SNSHandler = async (event: SNSEvent) => {
-  const records: SNSEventRecord[] = event.Records;
-  // For the sake of this tutorial I am just using a fire and forget with this forEach. If you want to try and catch you can use await Promise.all or something
-  records.forEach(record => {
-    console.log('Message is: ', record.Sns.Message);
-  });
+export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  try {
+    const parsedBody = JSON.parse(event.body || '');
+    return {
+      statusCode: 200,
+      body: `Goodbye ${parsedBody?.name}`,
+    };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: 'An error occured',
+    };
+  }
 };
--- src/functions/question.ts
+++ src/functions/question.ts
@@ -1,1 +1,12 @@
-{"version":3,"file":"hello.js","sourceRoot":"","sources":["../../../src/functions/hello.ts"],"names":[],"mappings":";;;;;;;;;;;;AAEO,MAAM,OAAO,GAAG,CAAO,MAA4B,EAAkC,EAAE;IAC5F,IAAI;QACF,MAAM,QAAQ,GAAG;YACf,UAAU,EAAE,GAAG;YACf,IAAI,EAAE,4BAA4B;SACnC,CAAC;QACF,OAAO,QAAQ,CAAC;KACjB;IAAC,OAAO,GAAG,EAAE;QACZ,OAAO;YACL,UAAU,EAAE,GAAG;YACf,IAAI,EAAE,kBAAkB;SACzB,CAAC;KACH;AACH,CAAC,CAAA,CAAC;AAbW,QAAA,OAAO,WAalB"}
+import { SNSHandler, SNSEvent, SNSEventRecord } from 'aws-lambda';
+
+/*
+    SNS Topics don't expect a return value. Usually you either publish to another topic or just do some work and finish
+*/
+export const handler: SNSHandler = async (event: SNSEvent) => {
+  const records: SNSEventRecord[] = event.Records;
+  // For the sake of this tutorial I am just using a fire and forget with this forEach. If you want to try and catch you can use await Promise.all or something
+  records.forEach(record => {
+    console.log('Message is: ', record.Sns.Message);
+  });
+};
